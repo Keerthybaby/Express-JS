62d1bde10e0051fcf66446eda6b59aedf9033ba8
Keerthy Baby <keerthybaby025@gmail.com>
2024-01-11 17:03:40 +0530

    Get_Requests

--- Get_Requests/src/index.mjs
+++ Get_Requests/src/index.mjs
@@ -11,7 +11,24 @@ const PORT =process.env.PORT || 3000;
     response.status(201).send({msg:"hello"});
   });
 
-  
+  app.get("/api/users",(request,response)=>{
+    response.send([
+      {
+        id:1,username:"anson",displayName:"Anson"
+      },
+      {
+        id:2,username:"Jack",displayName:"Jack"
+      },
+      {
+        id:3,username:"adam",displayName:"Adam"
+      }
+    ]);
+  });
+
+app.get("/api/products",(request,response)=>{
+  response.send([{id:123,name:"chicken breast",price:12.99}]);
+});
+
 app.listen(PORT,()=>{
   console.log(`Running on Port ${PORT}`);
 });
