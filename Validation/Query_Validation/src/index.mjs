b5a94e4305f6b526da65203d4c5eab41324c6439
Keerthy Baby <keerthybaby025@gmail.com>
2024-01-17 01:06:28 +0530

    Add Query Validation

---
+++ Validation/Query_Validation/src/index.mjs
@@ -0,0 +1,94 @@
+import express from "express";
+import { query, validationResult, body } from "express-validator";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// app.get("/",(request,response)=>{
+//   response.send("hello,World!");
+// });
+
+const mockUsers = [
+  {
+    id: 1,
+    username: "anson",
+    displayName: "Anson",
+  },
+  {
+    id: 2,
+    username: "Jack",
+    displayName: "Jack",
+  },
+  {
+    id: 3,
+    username: "adam",
+    displayName: "Adam",
+  },
+  {
+    id: 4,
+    username: "tina",
+    displayName: "Tina",
+  },
+  {
+    id: 5,
+    username: "jason",
+    displayName: "Jason",
+  },
+  {
+    id: 6,
+    username: "henry",
+    displayName: "Henry",
+  },
+  {
+    id: 7,
+    username: "marilyn",
+    displayName: "Marilyn",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.status(201).send({ msg: "hello" });
+});
+
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3-10 characters"),
+  (request, response) => {
+    const result = validationResult(request);
+    console.log(result);
+    const {
+      query: { filter, value },
+    } = request;
+
+    if (filter && value)
+      return response.send(
+        mockUsers.filter((user) => user[filter].includes(value))
+      );
+    return response.send(mockUsers);
+    // if(!filter && !value) return response.send(mockUsers);
+  }
+);
+
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId))
+    return response.status(400).send({ msg: "Bad Request. Invalid ID" });
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
+});
+
+app.get("/api/products", (request, response) => {
+  response.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+});
